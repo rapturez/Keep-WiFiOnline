bb1f19034f84a61e0a1d543bd618751c026f561f
RZ <RZ>
2015-11-24 08:18:12 +1100

    PS1 File from Computer
    Added PS1 file from computer.

---
+++ Test-WiFiConnectionIntegrity.ps1
@@ -0,0 +1,12 @@
+﻿$WirelessAdapters = Get-NetAdapter | Where-Object -Property PhysicalMediaType -ILike "*802.11*"
+
+function Test-ConnectionIntegrity($WF)
+    {
+        foreach ($w in $WF)
+        {
+            Test-Connection 8.8.8.8 -Count 1
+            "hi" 
+        }
+    }
+
+Test-ConnectionIntegrity($WirelessAdapters)
